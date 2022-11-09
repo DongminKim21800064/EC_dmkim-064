ae75889c0d86528b4e64ad0af70acd895102aed5
DongminKim21800064 <91419683+DongminKim21800064@users.noreply.github.com>
2022-11-10 02:38:26 +0900

    231

--- include/ecPWM.h
+++ include/ecPWM.h
@@ -1,8 +1,8 @@
 /**
 ******************************************************************************
-* @author  Dongmin Kim
-* @Mod		 2022-10-30 by Dongmin Kim  	
-* @brief   Embedded Controller 
+* @author  SSSLAB
+* @Mod		 2022-11-09 by Dongmin Kim  	
+* @brief   Embedded Controller:  EC_HAL
 * 
 ******************************************************************************
 */
@@ -33,6 +33,7 @@ void PWM_period_us(PWM_t *pwm, uint32_t usec);
 
 
 void PWM_pulsewidth_ms(PWM_t *pwm, float pulse_width_ms);
+void PWM_pulsewidth_us(PWM_t *pwm, float pulse_width_us);
 void PWM_duty(PWM_t *pwm, float duty);
 void PWM_pinmap(PWM_t *PWM_pin);
 
